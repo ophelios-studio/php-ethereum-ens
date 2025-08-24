d43e1e8af32f7ee70bdf3926d52d204257bb0db6
dtucker <davidt2003@msn.com>
2025-08-24 16:30:14 -0400

    refactor: add constant types

--- src/Ens/Configuration.php
+++ src/Ens/Configuration.php
@@ -2,12 +2,12 @@
 
 class Configuration
 {
-    public const DEFAULT_TIMEOUT_MS = 10000;
-    public const DEFAULT_REGISTERY_ADDRESS = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'; // ENS Registry mainnet address
+    public const int DEFAULT_TIMEOUT_MS = 10000;
+    public const string DEFAULT_REGISTRY_ADDRESS = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'; // ENS Registry mainnet address
 
     public function __construct(
         public readonly string $rpcUrl,
         public readonly int $timeoutMs = self::DEFAULT_TIMEOUT_MS,
-        public readonly string $registryAddress = self::DEFAULT_REGISTERY_ADDRESS
+        public readonly string $registryAddress = self::DEFAULT_REGISTRY_ADDRESS
     ) {}
 }
